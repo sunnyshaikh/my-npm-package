bc63373833358508d762c2b8843bfa30a0c95767
AltafAlam Shaikh <gitforaltaf@gmail.com>
2024-04-02 00:38:03 +0530

    init

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type ButtonProps = React.PropsWithChildren<
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+>;
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const { children, ...others } = props;
+  return (
+    <button
+      {...others}
+      style={{
+        padding: "1rem 1.5rem",
+        background: "#007bff",
+        color: "#fff",
+        border: 0,
+        outline: 0,
+        borderRadius: "6px",
+        cursor: "pointer",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export { Button };
